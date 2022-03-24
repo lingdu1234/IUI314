790de230b5a88eef5b29db9431badecc55f8bb77
lingdu <waong2005@126.com>
2022-03-24 18:32:12 +0800

    fix merge err

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,6 @@
 import { login, logout, getInfo } from '@/api/login'
-import { getToken, setToken, removeToken } from '@/utils/auth'
+import { freshToken} from '@/api/system/user'
+import { getToken, setToken, removeToken,setTokenExp,removeTokenExp,setTokenExpStatus } from '@/utils/auth'
 import defAva from '@/assets/images/profile.jpg'
 
 const user = {
@@ -8,6 +9,8 @@ const user = {
     name: '',
     avatar: '',
     roles: [],
+    role:'',
+    uid:'',
     permissions: []
   },
 
@@ -24,6 +27,12 @@ const user = {
     SET_ROLES: (state, roles) => {
       state.roles = roles
     },
+    SET_ROLE: (state, role) => {
+      state.role = role
+    },
+    SET_UID: (state, uid) => {
+      state.uid = uid
+    },
     SET_PERMISSIONS: (state, permissions) => {
       state.permissions = permissions
     }
@@ -38,7 +47,25 @@ const user = {
       const uuid = userInfo.uuid
       return new Promise((resolve, reject) => {
         login(username, password, code, uuid).then(res => {
-          setToken(res.token)
+          let exp = res.exp_in/1440 // 计算token过期时间 exp_in是分钟，除以1440转换成天
+          setTokenExp(res.exp)
+          setTokenExpStatus(1)
+          setToken(res.token,exp)
+          commit('SET_TOKEN', res.token)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    FreshToken({ commit }) {
+      return new Promise((resolve, reject) => {
+        freshToken().then(res => {
+          let exp = res.exp_in/1440 // 计算token过期时间 exp_in是分钟，除以1440转换成天
+          setTokenExp(res.exp)
+          setTokenExpStatus(1)
+          setToken(res.token,exp)
           commit('SET_TOKEN', res.token)
           resolve()
         }).catch(error => {
@@ -48,19 +75,20 @@ const user = {
     },
 
     // 获取用户信息
-    GetInfo({ commit, state }) {
+    GetInfo({ commit }) {
       return new Promise((resolve, reject) => {
         getInfo().then(res => {
           const user = res.user
           const avatar = (user.avatar == "" || user.avatar == null) ? defAva : import.meta.env.VITE_APP_BASE_API + user.avatar;
-
           if (res.roles && res.roles.length > 0) { // 验证返回的roles是否是一个非空数组
             commit('SET_ROLES', res.roles)
+            commit('SET_ROLE', res.user.role_id)
+            commit('SET_UID', res.user.id)
             commit('SET_PERMISSIONS', res.permissions)
           } else {
             commit('SET_ROLES', ['ROLE_DEFAULT'])
           }
-          commit('SET_NAME', user.userName)
+          commit('SET_NAME', user.user_name)
           commit('SET_AVATAR', avatar)
           resolve(res)
         }).catch(error => {
@@ -70,13 +98,14 @@ const user = {
     },
 
     // 退出系统
-    LogOut({ commit, state }) {
+    LogOut({ commit }) {
       return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
+        logout().then(() => {
           commit('SET_TOKEN', '')
           commit('SET_ROLES', [])
           commit('SET_PERMISSIONS', [])
           removeToken()
+          removeTokenExp()
           resolve()
         }).catch(error => {
           reject(error)
