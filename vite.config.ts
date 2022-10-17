56ff98ba102d50bde2d827addfe6d72cf018b4b9
lingdu <waong2005@126.com>
2022-10-17 20:43:33 +0800

    set title

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: lingdu waong2005@126.com
  * @Date: 2022-09-30 18:41:35
  * @LastEditors: lingdu waong2005@126.com
- * @LastEditTime: 2022-10-15 09:08:43
+ * @LastEditTime: 2022-10-17 20:42:35
  * @FilePath: \IUI314\vite.config.ts
  * @Description:
  */
@@ -16,57 +16,72 @@ import ElementPlus from 'unplugin-element-plus/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-import { defineConfig } from 'vite'
+import { type ConfigEnv, defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend' //为setup  主要用于定义名称;
 
+const getViteEnv = (mode: string, target: string) => {
+  return loadEnv(mode, process.cwd())[target]
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueSetupExtend(),
-    DefineOptions(),
-    Unocss(),
-    ElementPlus({
-      useSource: true,
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 9876,
-  },
-  build: {
-    sourcemap: false,
-    minify: 'terser',
-    outDir: './dist',
-    rollupOptions: {
-      input: {
-        index: fileURLToPath(new URL('index.html', import.meta.url)),
+export default ({ mode }: ConfigEnv) =>
+  defineConfig({
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueSetupExtend(),
+      DefineOptions(),
+      Unocss(),
+      ElementPlus({
+        useSource: true,
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [
+          fileURLToPath(new URL('./src/assets/icons', import.meta.url)),
+        ],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: getViteEnv(mode, 'VITE_APP_TITLE'),
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-    // terserOptions: {
-    //   compress: {
-    //     drop_console: true,
-    //     drop_debugger: true,
-    //   },
-    //   sourceMap: false,
-    // },
-  },
-})
+    server: {
+      port: 9876,
+    },
+    build: {
+      sourcemap: false,
+      minify: 'terser',
+      outDir: './dist',
+      rollupOptions: {
+        input: {
+          index: fileURLToPath(new URL('index.html', import.meta.url)),
+        },
+      },
+      // terserOptions: {
+      //   compress: {
+      //     drop_console: true,
+      //     drop_debugger: true,
+      //   },
+      //   sourceMap: false,
+      // },
+    },
+  })
