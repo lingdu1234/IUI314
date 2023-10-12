45a5afd946e0d0d5dacde1b8ae0df1e3bc755644
lingdu <waong2005@126.com>
2023-10-12 17:38:10 +0800

    update deps

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,18 @@ import presetUno from '@unocss/preset-uno'
 import { defineConfig } from '@unocss/vite'
 
 export default defineConfig({
-  exclude: ['node_modules', 'dist', '.git', 'public', 'build', './stats.html'],
+  content: {
+    pipeline: {
+      exclude: [
+        'node_modules',
+        'dist',
+        '.git',
+        'public',
+        'build',
+        './stats.html',
+      ],
+    },
+  },
   presets: [presetUno()],
   shortcuts: {},
   theme: {
