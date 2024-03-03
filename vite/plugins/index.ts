4d0aea01b8242c63815a39caa46df332a5ab3539
lingdu <waong2005@126.com>
2024-03-03 14:23:22 +0800

    fix

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,5 +1,5 @@
 export * from './setupAutoImport'
 export * from './setupElementPlus'
 export * from './setupHtmlPlugin'
-export * from './setupImportCDN'
+// export * from './setupImportCDN'
 export * from './setupSvgIcons'
