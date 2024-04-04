cf5bb1b0f4f39c2ed4f1bb56a75645ab1a86b7f6
lingdu <waong2005@126.com>
2024-04-04 11:40:36 +0800

    a

--- src/components/iui/iu-modal.vue
+++ src/components/iui/iu-modal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type PropType, ref } from 'vue'
+import { type PropType, computed, ref, watch } from 'vue'
 import type { FormInstance } from '@arco-design/web-vue'
 import { FormItemType, type IuFormField } from '@/types/base/iu-form'
 import { useFormUtil } from '@/hooks'
@@ -39,6 +39,11 @@ const isFullscreen = ref(false)
 const formLayout = ref<'horizontal' | 'vertical' | 'inline'>('horizontal')
 const modalFormRef = ref<FormInstance>()
 const useForm = useFormUtil()
+
+const contentHeight = computed(() => 'calc(var(--vh) * 100) - 150')
+watch(() => contentHeight.value, (v) => {
+  console.log(v)
+})
 
 function beforeClose() {
   useForm.formReset(modalFormRef.value)
@@ -102,65 +107,67 @@ function toggleFullScreen() {
       </div>
       <div />
     </template>
-    <a-form
-      ref="modalFormRef"
-      :model="formValue"
-      :layout="formLayout"
-      auto-label-width
-      class="m-b-10px"
-    >
-      <a-form-item
-        v-for="item in formItems"
-        :key="item.field"
-        :field="item.field"
-        :label="item.label"
-        :rules="item.rule"
-        :validate-trigger="item.validateTrigger"
+    <a-scrollbar :style="`max-height:${contentHeight};overflow: auto;`" class="m-20px">
+      <a-form
+        ref="modalFormRef"
+        :model="formValue"
+        :layout="formLayout"
+        auto-label-width
+        class="m-b-10px"
       >
-        <a-input
-          v-if="item.type === FormItemType.input"
-          v-model="formValue[item.field]"
-          :placeholder="item.placeholder"
-          :disabled="item.disabled"
-          style="width: 300px"
-        />
-        <a-select
-          v-if="item.type === FormItemType.select && item.selectOption"
-          v-model="formValue[item.field]"
-          :options="item.selectOption.dataOption"
-          :field-names="item.selectOption.dataOptionKey"
-          :placeholder="item.placeholder"
-          :allow-clear="item.selectOption.allowClear"
-          :multiple="item.selectOption.multiple"
-          :allow-search="item.selectOption.allowSearch"
-          style="width: 300px;position: relative"
-        />
-        <a-date-picker
-          v-if="item.type === FormItemType.datePicker"
-          v-model="formValue[item.field]"
-          style="width: 300px;"
-        />
-        <a-textarea
-          v-if="item.type === FormItemType.textarea"
-          v-model="formValue[item.field]"
-          allow-clear
-          style="width: 300px;"
-        />
-        <a-radio-group
-          v-if="item.type === FormItemType.radio && item.selectOption"
-          v-model="formValue[item.field]"
-          style="width: 300px;"
-          :options="item.selectOption.dataOption"
-          :disabled="item.disabled"
-        />
-        <a-input-number
-          v-if="item.type === FormItemType.inputNumber"
-          v-model="formValue[item.field]"
-          :mode="item.inputNumberMode"
-          style="width: 300px;"
-        />
-      </a-form-item>
-    </a-form>
+        <a-form-item
+          v-for="item in formItems"
+          :key="item.field"
+          :field="item.field"
+          :label="item.label"
+          :rules="item.rule"
+          :validate-trigger="item.validateTrigger"
+        >
+          <a-input
+            v-if="item.type === FormItemType.input"
+            v-model="formValue[item.field]"
+            :placeholder="item.placeholder"
+            :disabled="item.disabled"
+            style="width: 300px"
+          />
+          <a-select
+            v-if="item.type === FormItemType.select && item.selectOption"
+            v-model="formValue[item.field]"
+            :options="item.selectOption.dataOption"
+            :field-names="item.selectOption.dataOptionKey"
+            :placeholder="item.placeholder"
+            :allow-clear="item.selectOption.allowClear"
+            :multiple="item.selectOption.multiple"
+            :allow-search="item.selectOption.allowSearch"
+            style="width: 300px;position: relative"
+          />
+          <a-date-picker
+            v-if="item.type === FormItemType.datePicker"
+            v-model="formValue[item.field]"
+            style="width: 300px;"
+          />
+          <a-textarea
+            v-if="item.type === FormItemType.textarea"
+            v-model="formValue[item.field]"
+            allow-clear
+            style="width: 300px;"
+          />
+          <a-radio-group
+            v-if="item.type === FormItemType.radio && item.selectOption"
+            v-model="formValue[item.field]"
+            style="width: 300px;"
+            :options="item.selectOption.dataOption"
+            :disabled="item.disabled"
+          />
+          <a-input-number
+            v-if="item.type === FormItemType.inputNumber"
+            v-model="formValue[item.field]"
+            :mode="item.inputNumberMode"
+            style="width: 300px;"
+          />
+        </a-form-item>
+      </a-form>
+    </a-scrollbar>
   </a-modal>
 </template>
 
@@ -174,6 +181,9 @@ function toggleFullScreen() {
     .arco-modal-title-align-center {
       justify-content: space-between;
     }
+  }
+  .arco-modal-body{
+    padding: 0 !important;
   }
 }
 </style>
